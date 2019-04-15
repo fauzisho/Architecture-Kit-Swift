953fd52288f71963b9f377eb142d1e058ec398c6
fauzisho <fauzisholichin@gmail.com>
2019-04-15 15:15:47 +0700

    update router

--- VIPER Kit.xctemplate/Default/___FILEBASENAME___Presenter.swift
+++ VIPER Kit.xctemplate/Default/___FILEBASENAME___Presenter.swift
@@ -24,9 +24,9 @@ class ___VARIABLE_productName:identifier___Presenter: ___VARIABLE_productName:id
 
     weak private var view: ___VARIABLE_productName:identifier___ViewProtocol?
     var interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol?
-    private let router: ___VARIABLE_productName:identifier___WireframeProtocol
+    private let router: ___VARIABLE_productName:identifier___RouterProtocol
 
-    init(interface: ___VARIABLE_productName:identifier___ViewProtocol, interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol?, router: ___VARIABLE_productName:identifier___WireframeProtocol) {
+    init(interface: ___VARIABLE_productName:identifier___ViewProtocol, interactor: ___VARIABLE_productName:identifier___InteractorInputProtocol?, router: ___VARIABLE_productName:identifier___RouterProtocol) {
         self.view = interface
         self.interactor = interactor
         self.router = router
